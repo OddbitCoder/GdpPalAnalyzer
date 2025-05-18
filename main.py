7998ecd98cdab31a9163fa562dad6761ccc7bcdd
Oddbit Coder <oddbitcoder@gmail.com>
2025-05-18 16:09:39 +0200

    Reads all copies for comparing to originals (equal!)

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ class Mode(Enum):
     # 10L8
     READ_IC24 = 1
     CONVERT_IC24 = 2
-    VALIDATE_IC24 = 3
+    SIMULATE_IC24 = 3
     # 16L8
     READ_IC22 = 4
     CONVERT_IC22 = 5
-    VALIDATE_IC22 = 6
+    SIMULATE_IC22 = 6
     # 16L8 + HI-Z
     READ_IC7 = 7
     CONVERT_IC7 = 8
-    VALIDATE_IC7 = 9
+    SIMULATE_IC7 = 9
     # 16R4
     READ_IC12 = 10
     READ_IC12_EXT = 11
     CONVERT_IC12 = 12
-    VALIDATE_IC12 = 13
+    SIMULATE_IC12 = 13
     # 16R4 + HI-Z
     READ_IC49 = 14
     READ_IC49_EXT = 15
     CONVERT_IC49 = 16
-    VALIDATE_IC49 = 17
+    SIMULATE_IC49 = 17
 
 
 port = "COM4"
 delay = 0.0001
-mode = Mode.VALIDATE_IC49
+mode = Mode.READ_IC12
 path = "C:\\Work\\PalAnalyzer\\reads"
 
 if __name__ == "__main__":
@@ -40,8 +40,8 @@ if __name__ == "__main__":
         dupal_board = DuPalBoard(PalType.PAL10L8, port, delay)
         analyzer = PalLAnalyzer(dupal_board)
         analyzer.analyze(
-            f"{path}\\ic24\\ic24.txt",
-            f"{path}\\ic24\\ic24_stdout.txt",
+            f"{path}\\ic24\\ic24_copy.txt",
+            f"{path}\\ic24\\ic24_copy_stdout.txt",
         )
 
     if mode == Mode.CONVERT_IC24:
@@ -51,7 +51,7 @@ if __name__ == "__main__":
             pal_type=PalType.PAL10L8,
         )
 
-    if mode == Mode.VALIDATE_IC24:
+    if mode == Mode.SIMULATE_IC24:
         ic24 = IC24()
         analyzer = PalLAnalyzer(ic24)
         analyzer.analyze(
@@ -63,8 +63,8 @@ if __name__ == "__main__":
         dupal_board = DuPalBoard(PalType.PAL16L8, port, delay)
         analyzer = PalLAnalyzer(dupal_board)
         analyzer.analyze(
-            f"{path}\\ic22\\ic22.txt",
-            f"{path}\\ic22\\ic22_stdout.txt",
+            f"{path}\\ic22\\ic22_copy.txt",
+            f"{path}\\ic22\\ic22_copy_stdout.txt",
         )
 
     if mode == Mode.CONVERT_IC22:
@@ -74,7 +74,7 @@ if __name__ == "__main__":
             pal_type=PalType.PAL16L8,
         )
 
-    if mode == Mode.VALIDATE_IC22:
+    if mode == Mode.SIMULATE_IC22:
         ic22 = IC22()
         analyzer = PalLAnalyzer(ic22)
         analyzer.analyze(
@@ -86,8 +86,8 @@ if __name__ == "__main__":
         dupal_board = DuPalBoard(PalType.PAL16L8, port, delay)
         analyzer = PalLAnalyzer(dupal_board)
         analyzer.analyze(
-            f"{path}\\ic7\\ic7.txt",
-            f"{path}\\ic7\\ic7_stdout.txt",
+            f"{path}\\ic7\\ic7_copy.txt",
+            f"{path}\\ic7\\ic7_copy_stdout.txt",
         )
 
     if mode == Mode.CONVERT_IC7:
@@ -95,7 +95,7 @@ if __name__ == "__main__":
             f"{path}\\ic7\\ic7.txt", f"{path}\\ic7\\ic7.tbl", pal_type=PalType.PAL16L8
         )
 
-    if mode == Mode.VALIDATE_IC7:
+    if mode == Mode.SIMULATE_IC7:
         ic7 = IC7()
         analyzer = PalLAnalyzer(ic7)
         analyzer.analyze(
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     if mode == Mode.READ_IC12:
         dupal_board = DuPalBoard(PalType.PAL16R4, port, delay)
         analyzer = PalRAnalyzer(dupal_board)
-        analyzer.analyze(f"{path}\\ic12\\ic12.json")
+        analyzer.analyze(f"{path}\\ic12\\ic12_copy.json")
 
     if mode == Mode.READ_IC12_EXT:
         dupal_board = DuPalBoard(PalType.PAL16R4, port, delay)
@@ -120,7 +120,7 @@ if __name__ == "__main__":
             outputs_mask="OOOQQQQO0000000000",
         )
 
-    if mode == Mode.VALIDATE_IC12:
+    if mode == Mode.SIMULATE_IC12:
         ic12 = IC12()
         analyzer = PalRAnalyzer(ic12)
         analyzer.analyze(f"{path}\\ic12\\ic12_simulated.json")
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     if mode == Mode.READ_IC49:
         dupal_board = DuPalBoard(PalType.PAL16R4, port, delay)
         analyzer = PalRAnalyzer(dupal_board)
-        analyzer.analyze(f"{path}\\ic49\\ic49.json")
+        analyzer.analyze(f"{path}\\ic49\\ic49_copy.json")
 
     if mode == Mode.CONVERT_IC49:
         PalRAnalyzer.convert_file(
@@ -142,7 +142,7 @@ if __name__ == "__main__":
             inputs_mask="F0FFFFF00IIIIIIII0",  # pins 12 and 13 are inputs
         )
 
-    if mode == Mode.VALIDATE_IC49:
+    if mode == Mode.SIMULATE_IC49:
         ic49 = IC49()
         analyzer = PalRAnalyzer(ic49)
         analyzer.analyze(f"{path}\\ic49\\ic49_simulated.json")
